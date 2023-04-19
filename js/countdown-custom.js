7bd3f986d6dcd82744ba3423a2259cfd40f46b25
Adaeze Ndupu <andupu4@gmail.com>
2023-04-19 20:37:30 +0100

    initial commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2023, 6, 15, 11)}); // year, month, date, hour
+        });
+})(jQuery);
+
