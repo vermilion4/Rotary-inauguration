960422a1251d719f9b1f5ce7bb7d677f3f4a43ac
Adaeze Ndupu <andupu4@gmail.com>
2023-07-06 13:14:21 +0100

    update

--- js/unveil.js
+++ js/unveil.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', () => {
   const unveilBtn = document.querySelector('.btn-unveil');
+  const unveilBtn2 = document.querySelector('.btn-unveil2');
   const imageContainer = document.querySelector('.image-container img');
+  const imageContainer2 = document.querySelector('.image-container-2 img');
 
-  unveilBtn.addEventListener('click', () => {
-      unveilBtn.style.display = 'none';
-      imageContainer.style.display = 'block';
-  });
+  const handleClick = (button, imageContainer) => {
+    button.style.display = 'none';
+    imageContainer.style.display = 'block';
+  };
+
+  unveilBtn.addEventListener('click', () => handleClick(unveilBtn, imageContainer));
+  unveilBtn2.addEventListener('click', () => handleClick(unveilBtn2, imageContainer2));
 });
