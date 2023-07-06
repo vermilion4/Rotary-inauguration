f7569f912471b8e5cdce2b56e8153bfa566c9aef
Adaeze Ndupu <andupu4@gmail.com>
2023-07-06 12:50:06 +0100

    update site

--- js/unveil.js
+++ js/unveil.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
   const unveilBtn = document.querySelector('.btn-unveil');
-  const imageContainer = document.querySelector('.image-container');
+  const imageContainer = document.querySelector('.image-container img');
 
   unveilBtn.addEventListener('click', () => {
       unveilBtn.style.display = 'none';
